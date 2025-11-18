6a2ffaa1a5109d9d336fc30f43ee663f655964a0
Ramakrishna A <aruvaramakrishna@gmail.com>
2025-11-18 19:17:10 +0530

    Terraform

--- Terraform/ForEach/provider.tf
+++ Terraform/ForEach/provider.tf
@@ -14,7 +14,7 @@ provider "azurerm" {
   resource_provider_registrations = "none" # This is only required when the User, Service Principal, or Identity running Terraform lacks the permissions to register Azure Resource Providers.
   features {}
 
-  client_id       = "17b89dd2-baaa-450f-8f0b-e0ffaa82099a"
+  client_id       = ""
   client_secret   = "Ma48Q~wznP3n4S43SPLjgydodv-jYgUlfL_gycYR"
   tenant_id       = "459865f1-a8aa-450a-baec-8b47a9e5c904"
   subscription_id = "2e28c82c-17d7-4303-b27a-4141b3d4088f"
